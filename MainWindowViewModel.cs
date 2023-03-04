88bce4a7c4de25c1f7d6e5bf7f1303318105d567
Andrew <andyo.dev@gmail.com>
2023-03-04 11:21:28 +0000

    Add project files.

---
+++ MainWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_NavigationViewModelFirst
+{
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string mainWinVMString = "Hello from MainWindoViewModel";
+
+        [ObservableProperty]
+        private object currentViewModel = null;
+
+        [ObservableProperty]
+        List<TypeAndDisplay> navigationViewModelTypes = new List<TypeAndDisplay>
+            {
+               new TypeAndDisplay{ Name="Yellow", VMType= typeof(YellowViewModel) },
+               new TypeAndDisplay{ Name="Blue", VMType= typeof(BlueViewModel) }
+            };
+
+        [RelayCommand]
+        private void Navigate(Type vmType)
+        {
+            CurrentViewModel = null;
+            if (viewModels.ContainsKey(vmType))
+            {
+                CurrentViewModel = viewModels[vmType];
+                return;
+            }
+            CurrentViewModel = Activator.CreateInstance(vmType);
+            viewModels.Add(vmType, CurrentViewModel);
+        }
+
+        private Dictionary<Type, Object> viewModels = new Dictionary<Type, Object>();
+    }
+}
---
+++ TypeAndDisplay.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_NavigationViewModelFirst
+{
+    public class TypeAndDisplay : ObservableObject
+    {
+        public string Name { get; set; }
+        public Type VMType { get; set; }
+    }
+}
